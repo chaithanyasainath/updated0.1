43d42cfe601496c3d6c843d9e6925187e3c2926f
chaithanyasainath <108852932+chaithanyasainath@users.noreply.github.com>
2022-10-27 16:32:02 +0530

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -1,7 +1,9 @@
-import { Component, VERSION, OnInit } from '@angular/core';
+import { Component, VERSION, OnInit,ViewChild,ElementRef} from '@angular/core';
 import '@tensorflow/tfjs-backend-webgl';
 import * as blazeface from '@tensorflow-models/blazeface';
 import { angularMath } from 'angular-ts-math/dist/angular-ts-math/angular-ts-math';
+
+declare var MediaRecorder: any;
 
 @Component({
   selector: 'my-app',
@@ -9,13 +11,30 @@ import { angularMath } from 'angular-ts-math/dist/angular-ts-math/angular-ts-mat
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  
+  @ViewChild('recordedVideo') recordVideoElementRef: ElementRef;
+  @ViewChild('video') videoElementRef: ElementRef;
+
+
+ 
+
+
   name = 'Angular ' + VERSION.major;
   video!: any;
   state = 0;
   can!: any;
   ctx!: any;
+  videoElement!: HTMLVideoElement;
+  recordVideoElement!: HTMLVideoElement;
+  mediaRecorder: any;
+  recordedBlobs!: any;
+  isRecording: boolean = false;
+  downloadUrl!: any;
+  stream!: any;
+  videoBuffer!: any;
+  button! : any
  
+
+  constructor() {}
 
   async ngOnInit() {
     // navigator.permissions
@@ -35,13 +54,28 @@ export class AppComponent implements OnInit {
     //     };
     //   });
     
+    navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        width: 360
+      }
+    })
+    .then(stream => {
+      this.videoElement = this.videoElementRef.nativeElement;
+      this.recordVideoElement = this.recordVideoElementRef.nativeElement;
+
+      this.stream = stream;
+      this.videoElement.srcObject = this.stream;
+    });
+
+
     this.setupCamera();
     this.predictFace();
     
     this.video = document.getElementById('video');
     this.can = document.getElementById('canv');
     this.ctx = this.can.getContext("2d");
-    this.ctx.rect(110,100,510,400*2 );
+    this.ctx.rect(155,40,70,80);
     //this.ctx.ellipse(100, 100, 50, 70, 0, 0, angularMath.getPi() * 2);
     this.ctx.stroke();
     //this.can.drawImage(this.video ,150 ,150 , 50 , 50);
@@ -86,7 +120,7 @@ export class AppComponent implements OnInit {
       //   console.log('face not found');
       // }
 
-                
+                
       if ((await predictions).length > 0) {
         for (let i = 0; i < (await predictions).length; i++) {
           console.log((await predictions));
@@ -94,5 +128,75 @@ export class AppComponent implements OnInit {
       }
 
     }, 1000);
+  }
+  
+  startRecording() {
+    this.recordedBlobs = [];
+    let options: any = { mimeType: 'video/webm' };
+
+    try {
+      this.mediaRecorder = new MediaRecorder(this.stream, options);
+    } catch (err) {
+      console.log(err);
+    }
+
+    this.mediaRecorder.start(); // collect 100ms of data
+    this.isRecording = !this.isRecording;
+    this.onDataAvailableEvent();
+    this.onStopRecordingEvent();
+  }
+
+  stopRecording() {
+    this.mediaRecorder.stop();
+    this.isRecording = !this.isRecording;
+    console.log('Recorded Blobs: ', this.recordedBlobs);
+  }
+
+  playRecording() {
+    if (!this.recordedBlobs || !this.recordedBlobs.length) {
+      console.log('cannot play.');
+      return;
+    }
+    this.recordVideoElement.play();
+  }
+
+  onDataAvailableEvent() {
+    try {
+      this.mediaRecorder.ondataavailable = (event: any) => {
+        if (event.data && event.data.size > 0) {
+          this.recordedBlobs.push(event.data);
+        }
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
+  
+  onStopRecordingEvent() {
+    try {
+      this.mediaRecorder.onstop = (event: Event) => {
+        const videoBuffer = new Blob(this.recordedBlobs, {
+          type: 'video/webm'
+        });
+        
+          this.downloadUrl = window.URL.createObjectURL(videoBuffer);
+          this.recordVideoElement.src = this.downloadUrl;
+          console.log(window.URL.createObjectURL(videoBuffer))
+          
+          
+        
+        // this.downloadUrl = window.URL.createObjectURL(videoBuffer);
+        
+        // you can download with <a> tag
+        //this.recordVideoElement.src = this.downloadUrl;
+         //console.log("button()")
+        // console.log(window.URL.createObjectURL(videoBuffer))
+      };
+    } catch (error) {
+      console.log(error);
+    }
+   
   }
 }
